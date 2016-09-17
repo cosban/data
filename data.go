5539dfc25a5eb0e5caad73efb5d2e332c89cd92e
cosban <me@cosban.net>
2016-09-17 14:40:42 -0600

    Add some documentation and create some methods that do a bit more grunt work

--- data.go
+++ data.go
@@ -11,11 +11,14 @@ import (
 
 var db *sql.DB
 
+// Statement is a struct which represents a prepared statement.
+// Calling Prepare allows the user to not have to construct this manually.
 type Statement struct {
 	Query string
 	Args  []interface{}
 }
 
+// Connect takes in a valid psql connection string and opens the connection
 func Connect(connection string) {
 	var err error
 	db, err = sql.Open("postgres", connection)
@@ -24,6 +27,7 @@ func Connect(connection string) {
 	}
 }
 
+// SetString returns the value of i if it isn't null, otherwise returns otherwise
 func SetString(i interface{}, otherwise string) string {
 	check := i.(sql.NullString)
 	if check.Valid {
@@ -32,6 +36,7 @@ func SetString(i interface{}, otherwise string) string {
 	return otherwise
 }
 
+// SetInt returns the value of i if it isn't null, otherwise returns otherwise
 func SetInt(i interface{}, otherwise int) int {
 	n := i.(sql.NullInt64)
 	if n.Valid {
@@ -40,21 +45,37 @@ func SetInt(i interface{}, otherwise int) int {
 	return otherwise
 }
 
+// GetDateString returns a formatted date string
 func GetDateString(i interface{}) string {
 	date := i.(time.Time)
 	return fmt.Sprintf("%d-%02d-%02d", date.Year(), date.Month(), date.Day())
 }
 
+// GetDateTimeString returns a formatted date-time string
 func GetDateTimeString(i interface{}) string {
 	date := i.(time.Time)
 	hour, min, sec := date.Clock()
 	return fmt.Sprintf("%d-%02d-%02d %02d:%02d:%02d", date.Year(), date.Month(), date.Day(), hour, min, sec)
 }
 
+// Prepare creates a Statement which may be used in any of the Execute or Query functions
 func Prepare(query string, args ...interface{}) Statement {
 	return Statement{query, args}
 }
 
+// PrepareAndExecute performs the actions of Prepare and Execute a single statement
+// in one step rather than forcing the user to manually Prepare a statement.
+func PrepareAndExecute(query string, args ...interface{}) error {
+	return ExecuteTransaction(Statement{query, args})
+}
+
+// PrepareAndQuery performs the actions of Prepare and Query a single statement
+// in one step rather than forcing the user to manually Prepare a statement.
+func PrepareAndQuery(query string, args ...interface{}) (*sql.Rows, error) {
+	return Query(Statement{query, args})
+}
+
+// ExecuteTransaction allows the user to execute >= 1 statement in a single sql transaction
 func ExecuteTransaction(stmts ...Statement) error {
 	tx, err := db.Begin()
 	if err != nil {
@@ -70,6 +91,14 @@ func ExecuteTransaction(stmts ...Statement) error {
 	return tx.Commit()
 }
 
+// Query allows the user to perform a query where >= 1 rows are expected back
+// It simply returns the rows so that the user may manipulate the data as they please
+func Query(stmt Statement) (*sql.Rows, error) {
+	return db.Query(stmt.Query, stmt.Args...)
+}
+
+// QueryRow allows the user to perform an sql query where only one row is expected
+// The results of the query are put into the passed in data interfaces
 func QueryRow(stmt Statement, data ...interface{}) error {
 	row, err := Query(stmt)
 	defer row.Close()
@@ -81,8 +110,4 @@ func QueryRow(stmt Statement, data ...interface{}) error {
 		err = row.Scan(data...)
 	}
 	return err
-}
-
-func Query(stmt Statement) (*sql.Rows, error) {
-	return db.Query(stmt.Query, stmt.Args...)
 }
